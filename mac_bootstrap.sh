ca7a03370135ade607f8f345a99821fa961fb4b4
Steven Schmidt <swschmidt@gmail.com>
2020-05-12 12:16:43 -0400

    Create mac_bootstrap.sh

---
+++ mac_bootstrap.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+#install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#Apps to install
+apps=(
+  visual-studio-code
+  github
+  iterm2
+  powershell
+  brave-browser
+  microsoft-edge
+  skitch
+  firefox
+  sublime-text
+)
+
+#ToBeAdded
+#Tor
+#Automox
+
+
+#Formula to install
+formula=(
+  git
+  htop
+)
+
+#Retired Formula
+#
+
+# Install apps
+echo "Installing apps..."
+read -p "Press any key to continue... " -n1 -s
+brew cask install --appdir="/Applications" ${apps[@]}
+
+# Install formula
+echo "Installing formula..."
+read -p "Press any key to continue... " -n1 -s
+brew install ${formula[@]}
